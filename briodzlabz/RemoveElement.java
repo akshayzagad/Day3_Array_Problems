2250cd713bbfa14b23b5111de4dcd6290af9f6ea
akshayzagad <akshayzagade62@gmail.com.com>
2022-11-18 13:49:56 +0530

    Day 3 Array Problems

--- briodzlabz/RemoveElement.java
+++ briodzlabz/RemoveElement.java
@@ -2,14 +2,14 @@ package com.briodzlabz;
 
 import java.util.Scanner;
 
-public class DeletElement {
+public class RemoveElement {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the size of element");
-        int size= scanner.nextInt();
-        int []Array=new int[size];
+        int size = scanner.nextInt();
+        int[] Array = new int[size];
         System.out.println(Array.length);
-        for (int i=0;i<size;i++) {
+        for (int i = 0; i < size; i++) {
             System.out.println("Enter the  element");
             Array[i] = scanner.nextInt();
         }
@@ -17,17 +17,15 @@ public class DeletElement {
         System.out.println(Array[1]);
         System.out.println(Array[2]);
         System.out.println("Enter the location");
-        int loc=scanner.nextInt();
-        for (int i=loc;i<=loc-1;i++){
-            Array[i]=Array[i+1];
-            System.out.println(Array[0]);
-            System.out.println(Array[1]);
-            System.out.println(Array[2]);
+        int loc = scanner.nextInt();
+
+
+        for (int i = loc; i < loc-1; i++) {
+            Array[i] = Array[i + 1];
         }
         size--;
         for (int i=0;i<size;i++) {
-            Array[i] = scanner.nextInt();
-            System.out.println(Array[i] + " ");
+            System.out.println(Array[i]);
         }
     }
 }
---
+++ briodzlabz/SortingArrayAsending.java
@@ -0,0 +1,31 @@
+package com.briodzlabz;
+
+import java.util.Scanner;
+
+public class SortingArrayAsending {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The Lenght");
+        int Lenght = scanner.nextInt();
+        int[] Array = new int[Lenght];
+        System.out.println("Enter the Array Element");
+        for (int i = 0; i < Array.length; i++)
+        {
+            Array[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < Lenght; i++)
+        {
+            for (int j = i + 1; j < Lenght; j++)
+            {
+                if (Array[i] > Array[j])
+                {
+                    int Temp = Array[i];
+                    Array[i] = Array[j];
+                    Array[j] = Temp;
+                }
+            }
+            System.out.println(Array[i]);
+        }
+    }
+}
